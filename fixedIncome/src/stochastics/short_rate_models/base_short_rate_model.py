b0269cafd2ff46e9af331c354756c66705b8016f
Alexander Lapanowski <aflapan@gmail.com>
2023-12-03 14:13:37 -0500

    Refactored the short rate models to modularize the compuations of integrating the short rate sample path, constructing the compounded accrual path, and constructing the discount curve. New image displaying the convexity correction needed for the Vasicek model.

--- fixedIncome/src/stochastics/short_rate_models/base_short_rate_model.py
+++ fixedIncome/src/stochastics/short_rate_models/base_short_rate_model.py
@@ -1,4 +1,4 @@
-from datetime import datetime, time
+from datetime import date, datetime, time
 from dateutil.relativedelta import relativedelta
 import numpy as np
 import math
@@ -10,7 +10,7 @@ from fixedIncome.src.scheduling_tools.day_count_calculator import DayCountCalcul
 from fixedIncome.src.stochastics.brownian_motion import datetime_to_path_call, BrownianMotion
 from fixedIncome.src.scheduling_tools.day_count_calculator import DayCountConvention
 from fixedIncome.src.scheduling_tools.scheduler import Scheduler
-from fixedIncome.src.curves.base_curve import DiscountCurve, KnotValuePair
+from fixedIncome.src.curves.base_curve import DiscountCurve, KnotValuePair, Curve
 from fixedIncome.src.curves.curve_enumerations import CurveIndex, EndBehavior, InterpolationMethod
 
 class DriftDiffusionPair(NamedTuple):
@@ -27,11 +27,12 @@ class ShortRateModel:
                  ) -> None:
         self.drift_diffusion_collection = drift_diffusion_collection
         self.brownian_motion = brownian_motion
-        self._discount_factor_snap_time: time = time(17, 0, 0, 0)  # 5 PM Snap time
-        self._path = None
         self._dt = dt
         self._rate_index = 0  # row index of the self.path numpy array which corresponds to the short rate
-                             # Subclass models will often generate multiple different intermediary rates/values
+                              # Subclass models will often generate multiple different intermediary rates/values
+        self._path = None
+        self._integrated_path = None
+        self._continuously_compounded_accrual_path = None
         self._discount_curve = None
 
     @property
@@ -46,8 +47,18 @@ class ShortRateModel:
         return self._dt
 
     @property
+    def integrated_path(self) -> np.array:
+        return self._integrated_path
+
+    @property
     def discount_curve(self) -> Optional[DiscountCurve]:
         return self._discount_curve
+
+    def _reset_paths_and_curves(self) -> None:
+        self._path = None
+        self._integrated_path = None
+        self._continuously_compounded_accrual_path = None
+        self._discount_curve = None
 
     def set_dt(self, new_dt: float) -> None:
         """ Sets a new increment dt. The old path is set to None because any path
@@ -83,24 +94,26 @@ class ShortRateModel:
         SDE collection provided when the object was instantiated.
         """
 
-
-    def discount_curve(self, start_date_time: Optional[datetime] = None,
-                       end_date_time: Optional[datetime] = None,
-                       curve_index: CurveIndex = CurveIndex.NONE,
-                       day_count_convention: DayCountConvention = DayCountConvention.ACTUAL_OVER_ACTUAL) -> DiscountCurve:
-
+    def generate_integrated_path(self,
+                        start_date_time: Optional[datetime] = None,
+                        end_date_time: Optional[datetime] = None,
+                        datetimes: Optional[list[datetime | date]] = None,
+                        ) -> np.array:
+        """
+        """
         if self.path is None:
             raise ValueError("Path is currently None. First generate a sample path of spot rates.")
 
         start_date_time = self.brownian_motion.start_date_time if start_date_time is None else start_date_time
         end_date_time = self.brownian_motion.end_date_time if end_date_time is None else end_date_time
-
-        datetimes = Scheduler.generate_dates_by_increments(start_date=start_date_time,
-                                                           end_date=end_date_time,
-                                                           increment=relativedelta(seconds=self.dt * DayCountCalculator.SECONDS_PER_YEAR),
-                                                           max_dates=1_000_000)
+        if datetimes is None:
+            datetimes = Scheduler.generate_dates_by_increments(start_date=start_date_time,
+                                                               end_date=end_date_time,
+                                                               increment=relativedelta(seconds=self.dt * DayCountCalculator.SECONDS_PER_YEAR),
+                                                               max_dates=1_000_000)
         running_integral = 0.0
-        interpolation_values = [KnotValuePair(start_date_time, 1.0)]
+        interpolation_values = [0.0]
+
         for start_dt, end_dt in itertools.pairwise(datetimes):
             start_rate, end_rate = self(start_dt)[self.rate_index], self(end_dt)[self.rate_index]
             min_rate, max_rate = min(start_rate, end_rate), max(start_rate, end_rate)
@@ -111,18 +124,79 @@ class ShortRateModel:
                 next_trapezoid_val = min_rate * self.dt + (max_rate - min_rate) * self.dt / 2
             # compute integral
             running_integral += next_trapezoid_val
-            interpolation_values.append(KnotValuePair(end_dt, math.exp(-running_integral)))
+            interpolation_values.append(running_integral)
 
-        if self._discount_curve is None:
-            self._discount_curve = DiscountCurve(interpolation_values=interpolation_values,
-                                                 interpolation_method=InterpolationMethod.LINEAR,
-                                                 index=curve_index,
-                                                 interpolation_day_count_convention=day_count_convention,
-                                                 reference_date=self.brownian_motion.start_date_time,
-                                                 left_end_behavior=EndBehavior.ERROR,
-                                                 right_end_behavior=EndBehavior.ERROR)
-        else:
-            self._discount_curve.reset_interpolation_values(interpolation_values)
+        self._integrated_path = np.array(interpolation_values)
+        return self._integrated_path
+
+    def accrual_curve(self,
+                      start_date_time: Optional[datetime] = None,
+                      end_date_time: Optional[datetime] = None,
+                      datetimes: Optional[list[datetime | date]] = None,
+                      day_count_convention: DayCountConvention = DayCountConvention.ACTUAL_OVER_ACTUAL
+                      ) -> Curve:
+        """
+        """
+        if self.path is None:
+            raise ValueError("Path is currently None. First generate a sample path of spot rates.")
+
+        start_date_time = self.brownian_motion.start_date_time if start_date_time is None else start_date_time
+        end_date_time = self.brownian_motion.end_date_time if end_date_time is None else end_date_time
+        if datetimes is None:
+            datetimes = Scheduler.generate_dates_by_increments(start_date=start_date_time,
+                                                               end_date=end_date_time,
+                                                               increment=relativedelta(seconds=self.dt * DayCountCalculator.SECONDS_PER_YEAR),
+                                                               max_dates=1_000_000)
+
+        if self._integrated_path is None:
+            self.generate_integrated_path(start_date_time, end_date_time, datetimes)
+
+        compound_interpolation_values = [KnotValuePair(start_date_time, 1.0)]
+        compound_interpolation_values += [KnotValuePair(datetime_obj, math.exp(integral))
+                                          for datetime_obj, integral in zip(datetimes, self._integrated_path)]
+
+        self._continuously_compounded_accrual_curve = Curve(interpolation_values=compound_interpolation_values,
+                                                            interpolation_method=InterpolationMethod.LINEAR,
+                                                            interpolation_day_count_convention=day_count_convention,
+                                                            reference_date=start_date_time,
+                                                            left_end_behavior=EndBehavior.ERROR,
+                                                            right_end_behavior=EndBehavior.ERROR)
+
+        return self._continuously_compounded_accrual_curve
+
+
+    def discount_curve(self,
+                       start_date_time: Optional[datetime] = None,
+                       end_date_time: Optional[datetime] = None,
+                       datetimes: Optional[list[datetime | date]] = None,
+                       curve_index: CurveIndex = CurveIndex.NONE,
+                       day_count_convention: DayCountConvention = DayCountConvention.ACTUAL_OVER_ACTUAL) -> DiscountCurve:
+
+        if self.path is None:
+            raise ValueError("Path is currently None. First generate a sample path of spot rates.")
+
+        start_date_time = self.brownian_motion.start_date_time if start_date_time is None else start_date_time
+        end_date_time = self.brownian_motion.end_date_time if end_date_time is None else end_date_time
+        if datetimes is None:
+            datetimes = Scheduler.generate_dates_by_increments(start_date=start_date_time,
+                                                               end_date=end_date_time,
+                                                               increment=relativedelta(seconds=self.dt * DayCountCalculator.SECONDS_PER_YEAR),
+                                                               max_dates=1_000_000)
+
+        if self._integrated_path is None:
+            self.generate_integrated_path(start_date_time, end_date_time, datetimes)
+
+        discount_interpolation_values = np.exp(-self._integrated_path)
+        discount_knot_values = [KnotValuePair(dateobj, df)
+                                for dateobj, df in zip(datetimes, discount_interpolation_values)]
+
+        self._discount_curve = DiscountCurve(interpolation_values=discount_knot_values,
+                                             interpolation_method=InterpolationMethod.LINEAR,
+                                             index=curve_index,
+                                             interpolation_day_count_convention=day_count_convention,
+                                             reference_date=self.brownian_motion.start_date_time,
+                                             left_end_behavior=EndBehavior.ERROR,
+                                             right_end_behavior=EndBehavior.ERROR)
 
         return self._discount_curve
 
--- fixedIncome/src/stochastics/short_rate_models/one_factor_models/vasicek_model.py
+++ fixedIncome/src/stochastics/short_rate_models/one_factor_models/vasicek_model.py
@@ -5,7 +5,7 @@ Reference, *Fixed Income Securities, 4th Ed.* by Tuckman and Serrat, page 205.
 Unit tests are contained in
 fixedIncome.tests.test_stochastics.test_short_rate_models.test_one_factor_models.test_vasicek_model.py
 """
-from datetime import datetime
+from datetime import datetime, date, time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -71,6 +71,9 @@ class VasicekModel(ShortRateModel):
             self, starting_value: np.ndarray | float, set_path: bool = True, seed: Optional[int] = None
     ) -> np.ndarray:
         """ Generates the Vasicek solution path through the Euler Discretization method. """
+
+        self._reset_paths_and_curves()
+
         drift_fxcn, diffusion_fxcn = self.drift_diffusion_pair
         brownian_increments = self.brownian_motion.generate_increments(dt=self.dt, seed=seed).flatten()
         solution = np.empty((1,  len(brownian_increments)+1))
@@ -119,7 +122,11 @@ if __name__ == '__main__':
     from fixedIncome.src.scheduling_tools.scheduler import Scheduler
 
     start_time = datetime(2023, 10, 15, 0, 0, 0, 0)
-    end_time = datetime(2073, 10, 15, 0, 0, 0, 0)
+    end_time = datetime(2123, 10, 15, 0, 0, 0, 0)
+    dates = Scheduler.generate_dates_by_increments(start_date=start_time,
+                                                   end_date=end_time,
+                                                   increment=timedelta(1),
+                                                   max_dates=1_000_000)
 
     vm = VasicekModel(long_term_mean=0.04,
                       reversion_speed=2.0,
@@ -128,25 +135,21 @@ if __name__ == '__main__':
                       end_date_time=end_time)
 
     path = vm.generate_path(starting_value=0.08, set_path=True, seed=1)
+    admissible_dates = [date_obj for date_obj in dates if date_obj < vm.end_date_time]
+
     vm.plot()
     plt.savefig('../../../../../../fixedIncome/docs/images/Vasicek_Short_Rate.png')
     plt.show()
 
+    # DISCOUNT CURVES
     NUM_CURVES = 20
     plt.figure(figsize=(13, 5))
     for seed in range(NUM_CURVES):
         vm.generate_path(starting_value=0.08, set_path=True, seed=seed)
         vm_df_curve = vm.discount_curve()
-
-        dates = Scheduler.generate_dates_by_increments(start_date=start_time,
-                                                       end_date=vm_df_curve.interpolation_values[-1].knot,
-                                                       increment=timedelta(1),
-                                                       max_dates=1_000_000)
-
-        discount_factors = [vm_df_curve(date_obj) for date_obj in dates]
-        plt.plot(dates, discount_factors, color='tab:blue', alpha=1, linewidth=0.5)
+        discount_factors = [vm_df_curve(date_obj) for date_obj in admissible_dates]
+        plt.plot(admissible_dates, discount_factors, color='tab:blue', alpha=1, linewidth=0.5)
         print(seed)
-
 
     plt.grid(alpha=0.25)
     plt.title(f'Discount Curves from Continuously-Compounding {NUM_CURVES} Vasicek Model Short Rate Paths\n'
@@ -154,5 +157,41 @@ if __name__ == '__main__':
               f'Volatility {vm.volatility}; Reversion Speed {vm.reversion_speed}')
     plt.ylabel('Discount Factor')
     plt.savefig('../../../../../../fixedIncome/docs/images/Vasicek_Discount_Curves.png')
+    plt.show()
+
+    # CONVEXITY
+    NUM_CURVES = 10_000
+    vm_avg_short_rate = np.zeros((1, len(admissible_dates)))
+    vm_avg_accrual = np.zeros((1, len(admissible_dates)))
+    vm_avg_integrated_path = np.zeros((1, len(admissible_dates)))
+
+    fig, ax = plt.subplots(1, 1, figsize=(13, 5))
+    for seed in range(NUM_CURVES):
+        vm.generate_path(starting_value=0.08, set_path=True, seed=seed)
+        vm.generate_integrated_path(datetimes=admissible_dates)
+        vm_avg_integrated_path += vm._integrated_path
+        vm_avg_accrual += np.exp(vm._integrated_path)
+        print(seed)
+
+    plt.plot(admissible_dates, vm_avg_accrual.flatten() / NUM_CURVES, color='tab:blue')
+    plt.plot(admissible_dates, np.exp(vm_avg_integrated_path.flatten() / NUM_CURVES), color='darkred')
+
+    plt.grid(alpha=0.25)
+    plt.title(f'Convexity in the Vasicek Model: Averaging {NUM_CURVES} Continuously-Compounded Short Rate Paths\n'
+              f'and Continuously-Compounding the Average of All Short Rate Paths'
+              f'\nModel Parameters: Mean {vm.long_term_mean}; Volatility {vm.volatility}; Reversion Speed {vm.reversion_speed}')
+    plt.ylabel('Accrual')
+    plt.legend(['Average Individual Compounded\nPath Accruals',
+                'Compounded Accrual of the\nAverage Short Rate Path'], loc='upper left', frameon=False)
+
+    ax2 = ax.twinx()
+    convexity_adjustment = vm_avg_accrual.flatten() / NUM_CURVES - np.exp(vm_avg_integrated_path.flatten() / NUM_CURVES)
+    plt.plot(admissible_dates, convexity_adjustment, color='grey', linestyle='--')
+
+    plt.legend(['Convexity Adjustment (Right)'],
+               loc='lower right', frameon=False)
+
+    ax2.set_ylabel('Difference')
+    plt.savefig('../../../../../../fixedIncome/docs/images/Vasicek_Convexity.png')
     plt.show()
 
